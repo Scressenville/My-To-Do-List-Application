b7407f32e820a103241b0bef58d9dbdcce83f764
Sylvain Cressenville <sylcressenville@gmail.com>
2023-03-02 17:16:03 +0100

    add task model class + bribes of code in main function

--- app/src/main/java/r411/project/todolistapplication/MainActivity.kt
+++ app/src/main/java/r411/project/todolistapplication/MainActivity.kt
@@ -1,11 +1,18 @@
 package r411.project.todolistapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import r411.project.todolistapplication.classes.TaskModelClass
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val emoji = String(Character.toChars(0x1F918))
+        val simpleDateFormat = SimpleDateFormat("dd-mm-yyyy HH:mm")
+        val test1: TaskModelClass = TaskModelClass(1, "Ménage", "🧹", "Passer le balais", simpleDateFormat.format(Date()), "A faire")
     }
 }
---
+++ app/src/main/java/r411/project/todolistapplication/classes/TaskModelClass.kt
@@ -0,0 +1,3 @@
+package r411.project.todolistapplication.classes
+
+class TaskModelClass(var taskId: Int, val taskCategory: String, val taskCategoryIcon: String, val taskDescription: String, val taskDeadLine: String?, val taskStatus: String)
